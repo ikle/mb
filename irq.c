6a2324a48c3ac29fe05e05eabaa792b6f1a720bb
Alexei A. Smekalkine <ikle@ikle.ru>
2023-08-07 06:34:28 +0300

    irq: show timer progress on terminal

--- irq.c
+++ irq.c
@@ -6,13 +6,26 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include <stdint.h>
+
+#include "arch/i386/io.h"
 #include "irq.h"
 
+void term_putc   (int c);
 void term_printf (const char *fmt, ...);
 
 void irq_entry (struct pt_regs st)
 {
+	static int ticks;
+
 	switch (st.trapno) {
+	case 32:
+		if (++ticks >= 1000) {
+			term_putc ('.');
+			ticks = 0;
+		}
+
+		return;
 	default:
 		term_printf ("W: unhandled interrupt %d, err = %d\n",
 			     st.trapno, st.err);
