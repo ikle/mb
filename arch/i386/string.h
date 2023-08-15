7a82820010eda947023131f6981b4cfab2beed19
Alexei A. Smekalkine <ikle@ikle.ru>
2023-08-15 23:54:49 +0300

    arch/i386/string: unify destination and source variable naming

--- arch/i386/string.h
+++ arch/i386/string.h
@@ -11,28 +11,28 @@
 
 #include <stddef.h>
 
-static inline void *memset (void *dest, int c, size_t count)
+static inline void *memset (void *dst, int c, size_t count)
 {
 	asm volatile (
 		"cld; rep stosb"
-		: "+D" (dest), "+c" (count)
+		: "+D" (dst), "+c" (count)
 		: "a" (c)
 		: "cc", "memory"
 	);
 
-	return dest;
+	return dst;
 }
 
-static inline void *memcpy (void *dest, const void *src, size_t count)
+static inline void *memcpy (void *dst, const void *src, size_t count)
 {
 	asm volatile (
 		"cld; rep movsb"
-		: "+D" (dest), "+S" (src), "+c" (count)
+		: "+D" (dst), "+S" (src), "+c" (count)
 		:
 		: "cc", "memory"
 	);
 
-	return dest;
+	return dst;
 }
 
 #endif  /* ARCH_I386_STRING_H */
